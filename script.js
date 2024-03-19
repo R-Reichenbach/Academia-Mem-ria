915405d6b68fc898470450a0b1d30d2e0ff7f9a8
R-Reichenbach <rafareichenbach@gmail.com>
2024-03-18 21:37:03 -0300

    first-commit

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $("#mobile_btn").on("click", function () {
+    $("#mobile_menu").toggleClass("active");
+    $("#mobile_btn").find("i").toggleClass("fa-x");
+  });
+});
+
+document
+  .getElementById("contactForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    let name = document.getElementById("name").value;
+    let message = document.getElementById("message").value;
+    let phoneNumber = "5518991099697"; // Número de telefone do WhatsApp
+
+    let url = `https://wa.me/5518998000585?text=Olá+meu+nome+é+${name}+e+${message}`;
+    window.open(url, "_blank");
+  });
